3919c42962958e8463321b3b098811bfbd2b95b8
Rusheel  Sandri <recca73@Rusheels-MacBook-Pro.local>
2017-09-21 16:36:09 -0700

    Adding initial Scripts and enemies

---
+++ ZombieRunner/Assets/Scripts/Eyes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour {
+
+	private Camera eyes ;
+
+	private float defaultView;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		eyes = GetComponent<Camera> ();
+
+		defaultView = eyes.fieldOfView;
+
+
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("zoom")) {
+				eyes.fieldOfView = defaultView / 1.5f;
+
+				Debug.Log ("Button V pressed");
+			} else {
+
+				eyes.fieldOfView = defaultView;
+			}
+
+	}
+}
--- ZombieRunner/Assets/Scripts/Player.cs
+++ ZombieRunner/Assets/Scripts/Player.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+
+	public bool timeToSpawn;
+
+	public Transform playerSpawnPoints;
+
+	private Transform[] spawnPoints;
+
+	private bool lastToggle;
+
+
+
+
 	// Use this for initialization
 	void Start () {
-		
+		spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (lastToggle != timeToSpawn) {
+			RespanwPlayer ();
+			timeToSpawn = false;
+
+		} else {
+			lastToggle = timeToSpawn;
+		 }
 		
+	}
+
+	private void RespanwPlayer  () {
+
+		int i = Random.Range (1, spawnPoints.Length);
+		transform.position = spawnPoints [i].transform.position;
+	
 	}
 }
