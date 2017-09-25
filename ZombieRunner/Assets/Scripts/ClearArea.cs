f5c6617df1eb207d41f31452707a9150cab83a90
Rusheel  Sandri <recca73@Rusheels-MacBook-Pro.local>
2017-09-25 16:48:43 -0700

    Initial elements

---
+++ ZombieRunner/Assets/Scripts/ClearArea.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearArea : MonoBehaviour {
+
+	public float timeSinceLastTrigger = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timeSinceLastTrigger += Time.deltaTime;
+
+		if (timeSinceLastTrigger > 2f) {
+		
+			Debug.Log ("In a clear area");
+			 
+		}
+	}
+
+	void OnTriggerStay (Collider coll) {
+
+		if (coll.gameObject.name != "Player") {
+		
+			timeSinceLastTrigger = 0f;
+		}
+	
+//		if (coll == null) {
+//		
+//		
+//			Debug.Log ("no collisisons detected");
+//		
+//		} else {
+//
+//			Debug.Log (coll.name);
+//			Debug.Log ("collisions Detected");
+//
+//		}
+//	
+	}
+}
---
+++ ZombieRunner/Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour {
+
+	public float dayTimeCycle;
+	private Quaternion initialSunRotation;
+
+	// Use this for initialization
+	void Start () {
+
+		initialSunRotation = transform.rotation;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float angleThisFrame = Time.deltaTime / 360;
+
+		transform.RotateAround (transform.position, Vector3.forward, angleThisFrame);
+
+
+	}
+}
